7c6ae4bbafcdf7f459aee6e48b099ccd496e5a6d
LiuBoyu <boyu.liu@gmail.com>
2016-07-25 11:53:57 +0800

    精简 cocos/network 模块 - 略微调整

--- cocos/network/Android.mk
+++ cocos/network/Android.mk
@@ -9,10 +9,9 @@ LOCAL_SRC_FILES := WebSocket.cpp
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../external/websockets/include/android \
-                    $(LOCAL_PATH)
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/..
 
-LOCAL_STATIC_LIBRARIES := cocos2dx_internal_static
-LOCAL_STATIC_LIBRARIES += libwebsockets_static
+LOCAL_STATIC_LIBRARIES := libwebsockets_static
 
 include $(BUILD_STATIC_LIBRARY)
